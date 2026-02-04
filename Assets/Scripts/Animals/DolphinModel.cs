fb83cbabd53ab0f9c14b784f1c014a8229d67cba
trashGameDev <thefreshkeeper@gmail.com>
2026-02-04 20:38:57 +1100

    Dolphins can now be cleaned, Dolphins can clean/dirty ocean tiles

--- Assets/Scripts/Animals/DolphinModel.cs
+++ Assets/Scripts/Animals/DolphinModel.cs
@@ -6,17 +6,29 @@ public class DolphinModel : MonoBehaviour
     public Rigidbody rb;
 
     public float speed;
-
     public float maxspeed;
+
+    [Header("Turning")]
+    public float turnSpeed = 360f; 
     
+    private bool isTurning = false;
+    private Quaternion targetRotation;
+
+    public LayerMask oceanTileLayer;
+    public LayerMask invisibleWallLayer;
+
+    public OilComponent oilComponent;
+
     void Update()
     {
-        MoveForwards();
+        if (!isTurning)
+            MoveForwards();
+        else
+            TurnTowardsTarget();
     }
-    
+
     void MoveForwards()
     {
-        
         rb.AddRelativeForce(Vector3.forward * speed, ForceMode.Acceleration);
 
         if (rb.linearVelocity.magnitude > maxspeed)
@@ -24,4 +36,51 @@ public class DolphinModel : MonoBehaviour
             rb.linearVelocity = rb.linearVelocity.normalized * maxspeed;
         }
     }
+
+    void TurnTowardsTarget()
+    {
+        transform.rotation = Quaternion.RotateTowards(
+            transform.rotation,
+            targetRotation,
+            turnSpeed * Time.deltaTime
+        );
+
+        if (Quaternion.Angle(transform.rotation, targetRotation) < 0.5f)
+        {
+            isTurning = false;
+        }
+    }
+
+    //when dolphin hits barrier, spin 180 degrees to turn around, then random between -75 and 75
+    void StartTurnAround()
+    {
+        float randomOffset = UnityEngine.Random.Range(-75f, 75f);
+        float turnAmount = 180f + randomOffset;
+
+        targetRotation = Quaternion.Euler(0f, transform.eulerAngles.y + turnAmount, 0f);
+        isTurning = true;
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (((1 << other.gameObject.layer) & invisibleWallLayer.value) != 0)
+        {
+            StartTurnAround();
+        }
+        else if(((1 << other.gameObject.layer) & oceanTileLayer.value) != 0)
+        {
+            Debug.Log("hit ocean tile");
+            if (oilComponent.IsOily)
+            {
+                OilComponent oil = other.gameObject.GetComponent<OilComponent>();
+                oil.Dirty();
+            }
+            else
+            {
+                OilComponent oil = other.gameObject.GetComponent<OilComponent>();
+                oil.Clean();
+            }
+        }
+    }
+
 }
--- Assets/Scripts/Animals/DolphinView.cs
+++ Assets/Scripts/Animals/DolphinView.cs
@@ -8,12 +8,23 @@ public class DolphinView : MonoBehaviour
     public Material oilMat;
 
     public Material cleanMat;
+
+    public OilComponent oilComponent;
     
     private void OnEnable()
     {
-        SetMaterials(cleanMat);
+        DirtyDolphin();
+        oilComponent.AnnounceCleanOrOily += SetDirtyClean;
     }
-    
+
+    private void SetDirtyClean(OilComponent arg1, bool arg2)
+    {
+        if (arg2)
+            CleanDolphin();
+        else
+            DirtyDolphin();
+    }
+
     public void DirtyDolphin()
     {
         SetMaterials(oilMat);
